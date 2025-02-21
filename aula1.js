c0eb7a7642528567b767c92d6715c265c8a9c659
Erickalinda <erickasmaniotto@gmail.com>
2025-02-21 11:52:29 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,13 @@
+// Calculadora em JavaScript
+const valor1 = 1;
+const valor2 = 2;
+let soma = valor1 + valor2;
+let sub = valor1 - valor2;
+let mult = valor1 * valor2;
+let div = valor1 / valor2;
+
+console.log("A soma e: " + soma);
+console.log("A subtracao e: " + sub);
+console.log("A multiplicacao e: " + mult);
+console.log("A divisao e: " + div) ;
+//node nome-arquivo.js
---
+++ boolean.js
@@ -0,0 +1,9 @@
+//BOOLEAN.JS
+let estudantes = true;
+let estudioss = true;
+if(estudantes == true){
+    console.log("Tem direito a meia entrada!");
+} else {
+    console.log("pague inteira");
+}
+//< < >= <= == 1= 
