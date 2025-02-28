d659b0698072b7c020e34b52cba0276fdcb95186
Erickalinda <erickasmaniotto@gmail.com>
2025-02-28 08:58:50 -0300

    Add files via upload

--- prova.js
+++ prova.js
@@ -1,19 +1,24 @@
-let av1 = 5.0;
-let av2 = 6.0;
-let recup1 = 7.0;
-let recup2 = 3.0;
-let nota1
-let nota2
-if(recup1>av1) {
-    nota1=recup1
-} else{
-    nota1= 
-}
-if(recup2>av2){
-    nota2=recup2
-} else {
-    nota2
-}
-if(nota1+nota2>=6.0){
-    
-}
+let av1= 5.0;
+let av2= 7.0;
+let rec1= 6.0;
+let rec2= 3.0;
+let nt1;
+let nt2;
+
+if(rec1 > av1 ){
+    nt1=rec1
+ }else{
+    nt1=av1
+ }
+ if(rec2 > av2 ){
+    nt2=rec2
+ }else{
+    nt2=av2
+ }
+ let total =nt1+nt2
+ if( total/2 >=6.0){
+    console.log("voce passo sua media foi " + total/2)
+ }else{
+    console.log ("voce reprovou sua media foi " + total/2)
+
+ }
